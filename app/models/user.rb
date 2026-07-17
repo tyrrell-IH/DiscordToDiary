db328417d2b18fb63a6af91d141cc5ffb86b9d48
tyrrell-IH <gotothertm@gmail.com>
2026-07-17 10:17:23 +0900

    Userモデルのバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
   enum :default_visibility, { unconfigured: 0, everyone: 1, members_only: 2, only_me: 3 }
+
+  validates :discord_user_name, presence: true
+  validates :discord_user_id, presence: true, uniqueness: true
+  validates :default_visibility, presence: true
 end
