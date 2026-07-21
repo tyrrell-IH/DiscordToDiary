fac0cbb99f8ea649d78c04abda83cd3c324992f1
tyrrell-IH <gotothertm@gmail.com>
2026-07-22 00:29:43 +0900

    Diaryモデルスペックの作成

--- spec/factories/diaries.rb
+++ spec/factories/diaries.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :diary do
-    date { "2026-07-20" }
-    visibility { 1 }
-    user { nil }
+    sequence(:date) { |n| Date.new(2026, 1, 1) + (n - 1).days }
+    visibility { :only_me }
+    association :user
   end
 end
--- spec/models/diary_spec.rb
+++ spec/models/diary_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Diary, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is invalid without a date" do
+    diary = FactoryBot.build(:diary, date: nil)
+
+    expect(diary).to be_invalid
+  end
+
+  it "is invalid with a duplicate date for same user" do
+    user = FactoryBot.create(:user)
+    date = Date.new(2026, 1, 1)
+
+    FactoryBot.create(:diary, user:, date:)
+    new_diary = FactoryBot.build(:diary, user:, date:)
+
+    expect(new_diary).to be_invalid
+    expect(new_diary.errors[:date]).to include("has already been taken")
+  end
+
+  it "is valid with the same date for a different user" do
+    date = Date.new(2026, 1, 1)
+
+    FactoryBot.create(:diary, date:)
+    new_diary = FactoryBot.build(:diary, date:)
+
+    expect(new_diary).to be_valid
+  end
+
+  it "is invalid without a visibility" do
+    diary = FactoryBot.build(:diary, visibility: nil)
+
+    expect(diary).to be_invalid
+  end
 end
