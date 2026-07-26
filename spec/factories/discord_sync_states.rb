306bc5ce788e097511227c723ee70decef7bab39
tyrrell-IH <gotothertm@gmail.com>
2026-07-27 01:10:16 +0900

    DiscordSyncStateモデルスペックの作成

--- spec/factories/discord_sync_states.rb
+++ spec/factories/discord_sync_states.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :discord_sync_state do
-    last_discord_message_id { "MyString" }
-    synced_at { "2026-07-27 00:21:40" }
-    singleton_key { "MyString" }
+    last_discord_message_id { "1234567890123456789" }
+    synced_at { Time.current }
+    singleton_key { DiscordSyncState::SINGLETON_KEY }
   end
 end
--- spec/models/discord_sync_state_spec.rb
+++ spec/models/discord_sync_state_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe DiscordSyncState, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is invalid without a singleton_key" do
+    discord_sync_state = FactoryBot.build(:discord_sync_state, singleton_key: nil)
+
+    expect(discord_sync_state).to be_invalid
+  end
+
+  it "is invalid with a duplicate singleton_key" do
+    FactoryBot.create(:discord_sync_state)
+
+    new_discord_sync_state = FactoryBot.build(:discord_sync_state)
+
+    expect(new_discord_sync_state).to be_invalid
+    expect(new_discord_sync_state.errors[:singleton_key]).to include("has already been taken")
+  end
 end
