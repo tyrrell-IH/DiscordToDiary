679cfd539f23f9473df53a07c8d415c3645cf3eb
tyrrell-IH <gotothertm@gmail.com>
2026-09-03 23:41:31 +0900

    GET /users/:user_id/diariesのシステムスペックの作成

--- spec/requests/diaries_spec.rb
+++ spec/requests/diaries_spec.rb
@@ -1,10 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe "Diaries", type: :request do
-  describe "GET /index" do
-    it "returns http success" do
-      get "/diaries/index"
-      expect(response).to have_http_status(:success)
+  describe "GET /users/:user_id/diaries" do
+    it "returns 200 ok response" do
+      user = FactoryBot.create(:user)
+      get user_diaries_path(user)
+      expect(response).to have_http_status(:ok)
     end
   end
 end
