239298d83bacc14188afb0b5310957eabf72342a
tyrrell-IH <gotothertm@gmail.com>
2026-09-04 12:34:36 +0900

    Diaryのモデルスペックについて、分類ごとにdescribeでまとめた

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,39 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it "destroys associated diaries when the user is destroyed" do
-    user = FactoryBot.create(:user)
-    diary = FactoryBot.create(:diary, user:)
+  describe "associations" do
+    it "destroys associated diaries when the user is destroyed" do
+      user = FactoryBot.create(:user)
+      diary = FactoryBot.create(:diary, user:)
 
-    user.destroy
+      user.destroy
 
-    expect(Diary.exists?(diary.id)).to be false
+      expect(Diary.exists?(diary.id)).to be false
+    end
   end
 
-  it "is invalid without a discord_user_name" do
-    user = FactoryBot.build(:user, discord_user_name: nil)
+  describe "validations" do
+    it "is invalid without a discord_user_name" do
+      user = FactoryBot.build(:user, discord_user_name: nil)
 
-    expect(user).to be_invalid
-  end
+      expect(user).to be_invalid
+    end
 
-  it "is invalid without a discord_user_id" do
-    user = FactoryBot.build(:user, discord_user_id: nil)
+    it "is invalid without a discord_user_id" do
+      user = FactoryBot.build(:user, discord_user_id: nil)
 
-    expect(user).to be_invalid
-  end
+      expect(user).to be_invalid
+    end
 
-  it "is invalid with a duplicate discord_user_id" do
-    FactoryBot.create(:user, discord_user_id: '123')
+    it "is invalid with a duplicate discord_user_id" do
+      FactoryBot.create(:user, discord_user_id: '123')
 
-    new_user = FactoryBot.build(:user, discord_user_id: '123')
+      new_user = FactoryBot.build(:user, discord_user_id: '123')
 
-    expect(new_user).to be_invalid
-    expect(new_user.errors[:discord_user_id]).to include("has already been taken")
-  end
+      expect(new_user).to be_invalid
+      expect(new_user.errors[:discord_user_id]).to include("has already been taken")
+    end
 
-  it "is invalid without a default_visibility" do
-    user = FactoryBot.build(:user, default_visibility: nil)
+    it "is invalid without a default_visibility" do
+      user = FactoryBot.build(:user, default_visibility: nil)
 
-    expect(user).to be_invalid
+      expect(user).to be_invalid
+    end
   end
 end
