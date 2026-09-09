7c8f56bf377c87b6af31e8c2c511b216cd472264
tyrrell-IH <gotothertm@gmail.com>
2026-09-09 11:56:25 +0900

    Userモデル.sync_with_discordのテストの作成

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -40,4 +40,48 @@ RSpec.describe User, type: :model do
       expect(user).to be_invalid
     end
   end
+
+  describe ".sync_with_discord" do
+    let(:auth_info) do
+      OmniAuth::AuthHash.new(
+        provider: "discord",
+        uid: "12345",
+        info: {
+          name: "test",
+          image: "https://example.com/image.png"
+        }
+      )
+    end
+
+    context "when the user doesn't exist" do
+      it "creates and returns a new user" do
+        user = nil
+
+        expect {
+          user = User.sync_with_discord(auth_info)
+        }.to change(User, :count).by(1)
+        expect(user).to have_attributes(
+                          discord_user_id: auth_info.uid,
+                          discord_user_name: auth_info.info.name,
+                          avatar_url: auth_info.info.image
+                        )
+      end
+    end
+
+    context "when the user exists" do
+      it "updates and returns the existing user" do
+        existing_user = FactoryBot.create(:user,
+                                          discord_user_id: auth_info.uid,
+                                          discord_user_name: "old_user_name",
+                                          avatar_url: "old/image.png")
+        returned_user = User.sync_with_discord(auth_info)
+
+        expect(existing_user).to eq(returned_user)
+        expect(returned_user).to have_attributes(
+                                   discord_user_name: auth_info.info.name,
+                                   avatar_url: auth_info.info.image
+                                 )
+      end
+    end
+  end
 end
