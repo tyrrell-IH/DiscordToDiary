5c218dd3f41ecb24273044e320c8c282cc41de09
tyrrell-IH <gotothertm@gmail.com>
2026-09-17 13:16:52 +0900

    last_discord_message_idを使った取得位置の制御

--- app/discord/import_messages.rb
+++ app/discord/import_messages.rb
@@ -1,9 +1,13 @@
 class ImportMessages
   def call
-    messages = MessageFetcher.new.call
+    sync_state = DiscordSyncState.find_or_create_by!(singleton_key: DiscordSyncState::SINGLETON_KEY)
+    messages = MessageFetcher.new(last_discord_message_id: sync_state.last_discord_message_id).call
 
-    messages.each do |message|
-      MessageImporter.new(message).call
+    messages.sort_by(&:id).each do |message|
+      ActiveRecord::Base.transaction do
+        MessageImporter.new(message).call
+        sync_state.update!(last_discord_message_id: message.id)
+      end
     end
   end
 end
